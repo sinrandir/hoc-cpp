99eb53c3145f1ecc9c40eb6eee55c033f5264910
sinrandir <proboy12100@gmail.com>
2026-06-01 01:18:25 +0700

    thấy đéo có gì

--- brocode/9-usefulmathrelatedfunction.cpp
+++ brocode/9-usefulmathrelatedfunction.cpp
@@ -13,7 +13,8 @@ int main() {
     //z = sqrt(x); dấu căn hay mũ 1/2
     //z = abs(-5); dấu giá trị tuyệt đối
     //z = round(x); // round là hàm làm tròn như toán học THPT
-    z = ceil(x); //đối với hàm này thì luôn làm tròn lên ko làm tròn xuống
+    //z = ceil(x); //đối với hàm ceil thì luôn làm tròn lên ko làm tròn xuống 3.2=4
+    //z = floor(x); ngược lại so với hàm ceil hàm này luôn làm tròn xuống 3.99=3
     cout << z;
     return 0;
 }
